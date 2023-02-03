a26d00dbc3844f5c23baa470fca69be666f414d0
Carlos Villegas <carlos.villegasch@gmail.com>
2023-02-02 21:27:03 -0500

    Base de datos postres_databse con tabla de calorias

---
+++ postres_database.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM postres
+LIMIT 0, 1000
+
+-- Date: 2023-02-02 21:25
+*/
+INSERT INTO `` (`id`,`nombre`,`calorias`) VALUES (1,'Flan',159);
+INSERT INTO `` (`id`,`nombre`,`calorias`) VALUES (2,'Brownie',237);
+INSERT INTO `` (`id`,`nombre`,`calorias`) VALUES (3,'Helado',262);
+INSERT INTO `` (`id`,`nombre`,`calorias`) VALUES (4,'Torta',284);
